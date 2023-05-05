eb11f1c8f9f1273d8ff6ce9c58ae5c2d60f893f2
STaR <132711540+STa-aR@users.noreply.github.com>
2023-05-05 16:51:48 -0400

    V1
    V1

---
+++ Plugin.cs
@@ -0,0 +1,32 @@
+﻿using BepInEx;
+using System;
+using Unity.Mathematics;
+using UnityEngine;
+using Utilla;
+
+namespace Shine
+{
+    /// <summary>
+    /// This is your mod's main class.
+    /// </summary>
+
+    /* This attribute tells Utilla to look for [ModdedGameJoin] and [ModdedGameLeave] */
+    [BepInPlugin(PluginInfo.GUID, PluginInfo.Name, PluginInfo.Version)]
+    public class Plugin : BaseUnityPlugin
+    {
+        void OnGameInitialized(object sender, EventArgs e)
+        {
+
+        }
+
+        void Update()
+        {
+            Material[] array = (Material[])Material.FindObjectsOfType(typeof(Material));
+            foreach (Material Material in array)
+            { 
+                Material.SetFloat("_Metallic", 1f);
+                Material.SetFloat("_Glossiness", 1f);
+            }
+        }
+    }
+}
---
+++ PluginInfo.cs
@@ -0,0 +1,12 @@
+﻿namespace Shine
+{
+    /// <summary>
+    /// This class is used to provide information about your mod to BepInEx.
+    /// </summary>
+    internal class PluginInfo
+    {
+        public const string GUID = "com.star.gorillatag.shine";
+        public const string Name = "Shine";
+        public const string Version = "1.0.0";
+    }
+}
