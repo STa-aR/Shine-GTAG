0cb34a1c34987cd84843dfa599bafa2f9df79f6e
STaR <132711540+STa-aR@users.noreply.github.com>
2023-05-05 21:19:34 -0400

    fixed the mod working in public lobbies
    sorry :(

--- Plugin.cs
+++ Plugin.cs
@@ -11,9 +11,13 @@ namespace Shine
     /// </summary>
 
     /* This attribute tells Utilla to look for [ModdedGameJoin] and [ModdedGameLeave] */
+    [ModdedGamemode]
+    [BepInDependency("org.legoandmars.gorillatag.utilla", "1.5.0")]
     [BepInPlugin(PluginInfo.GUID, PluginInfo.Name, PluginInfo.Version)]
     public class Plugin : BaseUnityPlugin
     {
+        bool inRoom;
+
         void OnGameInitialized(object sender, EventArgs e)
         {
 
@@ -21,12 +25,50 @@ namespace Shine
 
         void Update()
         {
+            if (inRoom) 
+            {
+                Material[] array = (Material[])Material.FindObjectsOfType(typeof(Material));
+                foreach (Material Material in array)
+                {
+                    Material.SetFloat("_Metallic", 1f);
+                    Material.SetFloat("_Glossiness", 1f);
+                }
+            }
+            else
+            {
+                Material[] array = (Material[])Material.FindObjectsOfType(typeof(Material));
+                foreach (Material Material in array)
+                {
+                    Material.SetFloat("_Metallic", 0f);
+                    Material.SetFloat("_Glossiness", 0.3f);
+                }
+            }
+        }
+
+        /* This attribute tells Utilla to call this method when a modded room is joined */
+        [ModdedGamemodeJoin]
+        public void OnJoin(string gamemode)
+        {
             Material[] array = (Material[])Material.FindObjectsOfType(typeof(Material));
             foreach (Material Material in array)
-            { 
+            {
                 Material.SetFloat("_Metallic", 1f);
                 Material.SetFloat("_Glossiness", 1f);
             }
+            inRoom = true;
+        }
+
+        /* This attribute tells Utilla to call this method when a modded room is left */
+        [ModdedGamemodeLeave]
+        public void OnLeave(string gamemode)
+        {
+            Material[] array = (Material[])Material.FindObjectsOfType(typeof(Material));
+            foreach (Material Material in array)
+            {
+                Material.SetFloat("_Metallic", 0f);
+                Material.SetFloat("_Glossiness", 0f);
+            }
+            inRoom = false;
         }
     }
 }
